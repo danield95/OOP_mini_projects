09bfababf9bb51bb6da878b0a9525e3befa2147a
Daniel Cristian Dumitrescu <dumitrescu_cristian95@yahoo.com>
2020-11-02 18:10:42 +0000

    first exercise

---
+++ 1.first_class/code.py
@@ -0,0 +1,24 @@
+import math
+
+class Point:
+    def __init__(self, x=0.0, y=0.0):
+        self.x = x
+        self.y = y
+
+    def distance_to_origin(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def reflect(self, axis):
+        if axis=='x':
+            self.y=-self.y
+        elif axis=='y':
+            self.x=-self.x
+        else:
+            print('error: axis can be either x or y!')
+
+
+pt = Point(x=3.0)
+pt.reflect('y')
+print((pt.x, pt.y))
+pt.y = 4.0
+print(pt.distance_to_origin())
